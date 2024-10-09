ce89feec29172592d1607d38900644f8a8f29486
Umit Akdag <akdag.umit@ichc.ro>
2024-10-09 14:07:46 +0300

    Update-3

---
+++ src/week_1_introJava/HelloWorld.java
@@ -0,0 +1,10 @@
+package week_1_introJava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Worl") ;
+        System.out.println();
+        System.out.println("Welcome to ICHC");
+    }
+
+}
---
+++ src/week_1_introJava/week_1.java
@@ -0,0 +1,27 @@
+package week_1_introJava;
+
+public class week_1 {
+    /*
+    Topics:
+        Comments & Escape Sequences
+        Variables
+        Primitive Castings, Concatenation, Arithmetic
+        Operators, Unary, Shorthand, Relational, Logical
+        Single If, If & Else
+        Multi-branch if, Nested If, Recap
+        Ternaries
+        Switch
+        Scanner
+        String Methods
+        If, Switch, Scanner
+        Custom Methods (Void, Parameter, Return, Overloading)
+        While & Do-While Loops
+        Nested Loops
+        Arrays Utility, For Each
+        Multi-dimensional Array + Array & Loops
+        Wrapper Classes
+        ArrayList
+        ---
+     */
+
+}
