03043d3aa03aab6a5b56062218c2487076a1c986
umitakdag <umitakdag@gmail.com>
2024-10-16 20:19:05 +0300

    Updated Diamond

---
+++ src/week_1_introJava/Diamond.java
@@ -0,0 +1,16 @@
+package week_1_introJava;
+public class Diamond {
+    public static void main(String[] args) {
+        System.out.println("            ^");
+        System.out.println("           / \\");
+        System.out.println("          / * \\");
+        System.out.println("         / * * \\");
+        System.out.println("        / * * * \\");
+        System.out.println("        ---------");
+        System.out.println("        \\ * * * /");
+        System.out.println("         \\ * * /");
+        System.out.println("          \\ * /");
+        System.out.println("           \\ /");
+        System.out.println("            V");
+    }
+}
