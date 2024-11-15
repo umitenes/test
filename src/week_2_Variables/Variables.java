44caa76d0bc96f09c5a0437db05d0119a5e75808
umitakdag <umitakdag@gmail.com>
2024-11-15 22:30:55 +0200

    topics added

--- src/week_2_Variables/Variables.java
+++ src/week_2_Variables/Variables.java
@@ -1,10 +1,35 @@
 package week_2_Variables;
 
 public class Variables {
-
     public static void main(String[] args) {
 
-        System.out.println("Hi");
-    }
+        int salary=120000;   //120_000;
+        long populationOfTheWorld=8_000_000_000L;
+        float diameter=13.45f;
+        double pi=3.14;
 
+        System.out.println("args = " + args);
+
+        System.out.println("hello"+salary);
+        System.out.println(salary+pi+"ter"+diameter);
+
+        int num;
+        double val=32.33;
+        num=(int)val;
+        System.out.println("num =\' " + num);
+
+       long x=9_999_999_999l;
+       int y= (int) x;
+        System.out.println(y);
+
+        int a=127;
+        byte b=(byte)a;
+        System.out.println(56d);
+        System.out.println("a = " + a);
+
+        int w;
+        byte z=100;
+        int ab=z;
+        
+    }
 }
---
+++ src/week_3_StringConcatenation/StudentInfo.java
@@ -0,0 +1,38 @@
+package week_3_StringConcatenation;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+         student_name = "Tamara";
+         age = 25;
+         gender = 'F';
+         schoolName = "Cydeo";
+         phoneNumber1 = "(123)-456-7891";
+         phoneNumber2 = "(124)-454-7891";
+         isFullTime = true;
+         GPA = 3.5;
+        //soutv
+        System.out.println("student_name = " + student_name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("schoolName = " + schoolName);
+        System.out.println("phoneNumber = " + phoneNumber1);
+        System.out.println("phoneNumber2 = " + phoneNumber2);
+        System.out.println("GPA = " + GPA);
+        System.out.println("isFullTime = " + isFullTime);
+
+
+    }
+
+}
+
+/*
+2. Create a class called StudentInfo, and declare variables for the followings:
+    			1. student name (String)
+    			2. age (int)
+    			3. gender (char)
+    			4. school name (String)
+    			5. phone number (String) (###)-###-####
+    			6. full time (boolean)
+
+ */
